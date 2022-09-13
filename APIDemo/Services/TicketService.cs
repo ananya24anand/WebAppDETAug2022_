bee54f271f9220de7dbddbd3b6518efdb32097a1
Etg1 <Etg1@69J0RG2-SHEL>
2022-09-13 18:13:55 +0530

    web api

---
+++ APIDemo/Services/TicketService.cs
@@ -0,0 +1,22 @@
+﻿using APIDemo.Models;
+
+namespace APIDemo.Services
+{
+    public class TicketService
+    {
+        static List<Ticket> Tickets { get; }
+        static int nextId = 3;
+        static TicketService()
+        {
+            Tickets = new List<Ticket>
+                {
+                    new Ticket { Id = 1, Name = "Movie 1", Price=200},
+                    new Ticket { Id = 2, Name = "Movie 2", Price=1500 }
+                };
+        }
+
+        public static List<Ticket> GetAll() => Tickets;
+        public static Ticket? Get(int id) => Tickets.FirstOrDefault(t => t.Id == id);
+
+    }
+}
